a9c7df923ec67a7e77c5d021e5e29aa0eac7fb01
Adil Hassan A K <adilhassan9995@gmail.com>
2026-07-12 02:48:08 +0530

    Delete backend directory

--- backend/routes/reports.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const billController = require('../controllers/billController');
-const { authorizeAdmin } = require('../middleware/auth');
-
-// Support stats query for admin reports dashboard
-router.get('/stats', authorizeAdmin, billController.getStats);
-
-module.exports = router;
