dec4eefa28fd5347155498ad05cbf3c50badc76f
Adil Hassan A K <adilhassan9995@gmail.com>
2026-07-12 02:26:02 +0530

    Delete frontend directory

--- frontend/src/services/authService.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './api';
-
-export const authService = {
-  login: api.login.bind(api),
-  getCurrentUser: api.getCurrentUser.bind(api),
-  updateProfile: api.updateProfile.bind(api),
-};
-
-export default authService;
--- frontend/src/services/billService.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './api';
-
-export const billService = {
-  createBill: api.createBill.bind(api),
-  getBills: api.getBills.bind(api),
-  getBillDetails: api.getBillDetails.bind(api),
-};
-
-export default billService;
